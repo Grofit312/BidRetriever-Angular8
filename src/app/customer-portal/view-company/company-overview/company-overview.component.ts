7ecc160b9a5b75f39b898d1afd4ff9331726c7fe
Dinesh Karan Rathore <dinesh_rathore@wizorbit.com>
2020-11-18 20:05:33 +0530

    Active Projects Grid on View Company Screen

--- src/app/customer-portal/view-company/company-overview/company-overview.component.ts
+++ src/app/customer-portal/view-company/company-overview/company-overview.component.ts
@@ -5,48 +5,56 @@ import { NotificationsService } from "angular2-notifications";
 import { ViewCompanyApi } from "../view-company.api.service";
 import { ActivatedRoute } from "@angular/router";
 import { MomentPipe } from "app/shared/pipes/moment.pipe";
+import { DxDataGridComponent } from 'devextreme-angular';
 
 @Component({
-  selector: "app-company-overview",
-  templateUrl: "./company-overview.component.html",
-  styleUrls: ["./company-overview.component.scss"],
+  selector: 'app-company-overview',
+  templateUrl: './company-overview.component.html',
+  styleUrls: ['./company-overview.component.scss'],
   providers: [MyCalendarApi, MomentPipe],
 })
 export class CompanyOverviewComponent implements OnInit {
-  @ViewChild("editCompanyModal", { static: false }) editCompanyModal;
+  @ViewChild('editCompanyModal', { static: false }) editCompanyModal;
+  @ViewChild('editProjectModal', { static: false }) editProjectModal;
+  @ViewChild('addContactModal', { static: false }) addContactModal;
   @ViewChild('addEventModal', { static: false }) addEventModal;
-  @ViewChild("grid", { static: false }) grid;
+  @ViewChild('projectGrid', { static: false }) projectGrid: DxDataGridComponent;
+  @ViewChild('employeeGrid', { static: false }) employeeGrid: DxDataGridComponent;
   lat = 51.678418;
   lng = 7.809007;
 
   noAddress = false;
   rowData: { timestamp: any; name: string; event: any }[];
-  columnDefs = [
-    {
-      headerName: "Date",
-      field: "timestamp",
-      sortable: true,
-      filter: true,
-      resizable: true,
-      editable: false,
-      minWidth: 250,
-    },
-    {
-      headerName: "Name",
-      field: "name",
-      sortable: true,
-      filter: true,
-      resizable: true,
-      editable: false,
-      minWidth: 250,
-    },
+  
+  projectGridColumns = [
+    {caption: 'project Id', dataField: 'project_id', minWidth:'100', allowEditing: false, visible: false},
+    {caption: 'Project Name',dataField: 'project_name',minWidth: 200,allowEditing: false},
+    {caption: 'Bid Date',dataField: 'project_bid_datetime',minWidth: 100, cellTemplate: 'dateCell', allowEditing: false},
+    {caption: 'Office Assigned',dataField: 'project_assigned_office_name',minWidth: 100,allowEditing: false},
+    {caption: 'Admin User',dataField: 'company_admin_displayname',minWidth: 100,allowEditing: false},
+    {caption: 'Stage',dataField: 'project_stage',minWidth: 100,allowEditing: false},
+    {caption: 'Value',dataField: 'project_value',minWidth: 100,allowEditing: false},
+  
   ];
+
+  employeeGridColumns = [
+    { dataField: 'contact_id', dataType: 'number', caption: 'Contact Id', visible: false, allowEditing: false },
+    { dataField: 'contact_last_first_name', caption: 'Name',  minWidth: 200, allowEditing: false },
+    { dataField: 'contact_title', caption: 'Title', minWidth: 100, allowEditing: false },
+    { dataField: 'contact_email', caption: 'Email', minWidth: 100, allowEditing: false },
+    { dataField: 'contact_phone', caption: 'Phone', minWidth: 100, allowEditing: false },
+    { dataField: 'contact_city_state', caption: 'Location', minWidth: 100, allowEditing: false },
+
+  ];
+
+  activeProjects=null;
+  employees=null;
 
   constructor(
     private _momentPipe: MomentPipe,
     public dataStore: DataStore,
     public calendarApi: MyCalendarApi,
-    private viewProjectApi: ViewCompanyApi,
+    private viewCompanyApi: ViewCompanyApi,
 
     private notificationService: NotificationsService,
     public route: ActivatedRoute
@@ -79,10 +87,43 @@ export class CompanyOverviewComponent implements OnInit {
     ;
     this.loadMap();
     this.loadDates();
+    this.loadProjects();
+    this.loadEmployees();
   }
+loadEmployees() {
+  this.employees = null;
 
+  this.viewCompanyApi.findCompanyContact(
+    this.dataStore.currentCompany.customer_id,
+     this.dataStore.currentCompany.company_id,
+     this.dataStore.currentCustomer ? (this.dataStore.currentCustomer['customer_timezone'] || 'eastern') : 'eastern'
+     )
+     .then((res: any[]) => {
+      this.employees = res.sort((a, b) =>
+      a.contact_last_first_name.toString().toLowerCase() > b.contact_last_first_name.toString().toLowerCase()
+       ? 1 : a.contact_last_first_name.toString().toLowerCase() < b.contact_last_first_name.toString().toLowerCase() 
+            ? -1 : 0);
+    })
+    .catch(err => {
+      this.notificationService.error('Error', err, { timeOut: 3000, showProgressBar: false });
+    }); 
+ 
+}
+  loadProjects() {
+    this.activeProjects = null;
+
+    this.viewCompanyApi.findProjects(this.dataStore.currentCompany.company_id)
+    .then((res: any[]) => {
+      this.activeProjects = res.sort((a, b) =>
+      a.project_bid_datetime > b.project_bid_datetime ? 1 : a.project_bid_datetime < b.project_bid_datetime ? -1 : 0);
+    })
+    .catch(err => {
+      this.notificationService.error('Error', err, { timeOut: 3000, showProgressBar: false });
+    });
+
+  }
   loadDates() {
-    
+
     const companyDates = [
       {
         timestamp: this.dataStore.currentCompany["create_datetime"],
@@ -190,6 +231,142 @@ export class CompanyOverviewComponent implements OnInit {
         showProgressBar: false,
       });
     }
+  }
+  onViewProject() {
+    
+  const { selectedRowKeys } = this.projectGrid;
+  if(selectedRowKeys === null) return;
+
+  if (selectedRowKeys.length === 0) {
+    this.notificationService.error('No Selection', 'Please select one project!', { timeOut: 3000, showProgressBar: false });
+    return;
+  } else if (selectedRowKeys.length > 1) {
+    this.notificationService.error('Multiple Selection', 'Please select a single project.', { timeOut: 3000, showProgressBar: false });
+    return;
+  } 
+  window.open(`/customer-portal/view-project/${selectedRowKeys[0]['project_id']}`, '_blank');
+
+  }
+  
+  addEmployeeGridMenuItems(e) {
+    if (!e.row) { return; }
+ 
+    if (e.row && e.row.rowType === 'data') {   // e.items can be undefined
+      if (!e.items) { e.items = []; }
+  
+      e.items.push(
+        {
+          type: 'normal',
+          text: 'View Contact',
+          onItemClick: () => this.onViewContact()
+        },
+        // {
+        //   type: 'normal',
+        //   text: 'Edit Contact',
+        //   onItemClick: () => this.onEditContact()
+        // },
+        {
+          type: 'normal',
+          text: 'Add Contact',
+          onItemClick: () => this.onAddContact()
+        },
+        {
+          type: 'normal',
+          text: 'Call Contact',
+          onItemClick: () => this.onCallContact()
+        },
+      
+      );
+  }
+  }
+  onCallContact() {
+    const { selectedRowKeys } = this.employeeGrid;
+    if(selectedRowKeys === null) return;
+
+    if (selectedRowKeys.length === 0) {
+      this.notificationService.error('No Selection', 'Please select one contact!', { timeOut: 3000, showProgressBar: false });
+      return;
+    } else if (selectedRowKeys.length > 1) {
+      this.notificationService.error('Multiple Selection', 'Please select a single contact.', { timeOut: 3000, showProgressBar: false });
+      return;
+    }
+
+    window.open('tel:' + selectedRowKeys[0]['contact_phone']);
+  }
+  toolbarRefreshGridAction() {
+    this.loadEmployees();
+  }
+  onViewContact(){
+    const { selectedRowKeys } = this.employeeGrid;
+    if(selectedRowKeys === null) return;
+
+    if (selectedRowKeys.length === 0) {
+      this.notificationService.error('No Selection', 'Please select one contact!', { timeOut: 3000, showProgressBar: false });
+      return;
+    } else if (selectedRowKeys.length > 1) {
+      this.notificationService.error('Multiple Selection', 'Please select a single contact.', { timeOut: 3000, showProgressBar: false });
+      return;
+    }
+    window.open(`/customer-portal/view-employee/${selectedRowKeys[0]['contact_id']}/overview`, '_blank');
+  }
+  onAddContact() {
+    this.addContactModal.initialize(this);
+  }
+  onEditContact() {
+    throw new Error("Method not implemented.");
+  }
+  addProjectGridMenuItems(e) {
+    if (!e.row) { return; }
+ 
+    if (e.row && e.row.rowType === 'data') {   // e.items can be undefined
+      if (!e.items) { e.items = []; }
+  
+      e.items.push(
+        {
+          type: 'normal',
+          text: 'View Project',
+          onItemClick: () => this.onViewProject()
+        },
+        {
+          type: 'normal',
+          text: 'Edit Project',
+          onItemClick: () => this.onEditProjectAction()
+        },
+        {
+          type: 'normal',
+          text: 'View Project Documents',
+          onItemClick: () => this.onViewProjectDocumentAction()
+        },
+      );
+  }
+}
+  onEditProjectAction() {
+    const { selectedRowKeys } = this.projectGrid;
+    if(selectedRowKeys === null) return;
+  
+    if (selectedRowKeys.length === 0) {
+      this.notificationService.error('No Selection', 'Please select one project!', { timeOut: 3000, showProgressBar: false });
+      return;
+    } else if (selectedRowKeys.length > 1) {
+      this.notificationService.error('Multiple Selection', 'Please select a single project.', { timeOut: 3000, showProgressBar: false });
+      return;
+    } 
+    const selectedRows = this.activeProjects.filter(({ project_id: projectId }) => selectedRowKeys.includes(projectId));
+    
+    this.editProjectModal.initialize(this, selectedRowKeys[0]);
+     
+  }
+  onViewProjectDocumentAction() {
+    const { selectedRowKeys } = this.projectGrid;
+    if (selectedRowKeys.length === 0) {
+      this.notificationService.error('No Selection', 'Please select one project!', { timeOut: 3000, showProgressBar: false });
+      return;
+    } else if (selectedRowKeys.length > 1) {
+      this.notificationService.error('Multiple Selection', 'Please select just one project!', { timeOut: 3000, showProgressBar: false });
+      return;
+    }
+    const { currentUser: { user_id: userId } } = this.dataStore;
+    window.open(`${window['env'].docViewerBaseUrl}?project_id=${selectedRowKeys[0]['project_id']}&user_id=${userId}`, '_blank');
   }
   onRefresh() {
     window.location.reload();
